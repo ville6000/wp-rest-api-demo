024e1932671952f0c4c033e83606536e40a6254b
Ville Viklund <ville@7am.fi>
2015-11-04 10:40:29 +0200

    Removed post count from category list.

--- src/assets/js/category-posts.controller.js
+++ src/assets/js/category-posts.controller.js
@@ -6,15 +6,6 @@
         .controller('CategoryPostsController', ['$scope', '$routeParams', 'PostService', CategoryPostsController]);
 
     function CategoryPostsController($scope, $routeParams, PostService) {
-        $scope.posts = [];
-        $scope.searchMade = false;
-        $scope.resultCount = 0;
-
-        $scope.$watch('posts', function () {
-            $scope.postCount = $scope.posts.length;
-            $scope.searchMade = true;
-        }, true);
-
         PostService.getPostsByCategory($routeParams.slug).then(function (response) {
             $scope.posts = response.data;
         });
